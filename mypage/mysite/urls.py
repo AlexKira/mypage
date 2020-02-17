d54e476859bbfe27440ddde81d66375b05a97135
AlexKira <alex0014kbr87@gmail.com>
2020-02-17 23:27:54 +0300

    KOLOTILA.

--- mypage/mysite/urls.py
+++ mypage/mysite/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
-#mysite files
+
+from django.urls import path, include
+
 from . import views
 app_name = 'mysite'
 
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('topics/<int:topic_id>/', views.topic, name='topic'),
+
+    path('', views.index, name='index'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
--- mypage/mysite/views.py
+++ mypage/mysite/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render
 from .models import Topic
 
 def index(request):
-
-    return render(request,'mysite/index.html')
+	topics = Topic.objects.order_by('-date_added')
+	context = {
+		'topics': topics,
+	}
+	return render(request,
+		'mysite/index.html', context)
 
 def topic(request, topic_id):
-
-    return render(request,'mysite/topic.html')
+	topic = Topic.objects.get(id=topic_id)
+	entries = topic.entry_set.order_by('-date_added')
+	context = {
+		'topic': topic,
+		'entries': entries,
+	}
+	return render(request,
+		'mysite/topic.html', context)
